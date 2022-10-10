b057c83e34d48f4d721c676e739fcf0c473e7216
Nicholas Finefter <nfinefter@gmail.com>
2022-10-10 16:49:57 -0700

    Started Bellman Ford

--- WeightedDirectedGraphs/PathFinding.cs
+++ WeightedDirectedGraphs/PathFinding.cs
@@ -48,6 +48,87 @@ namespace WeightedDirectedGraphs
             float dy = Math.Abs(start.Y - end.Y);
             return 1 * (float)Math.Sqrt(dx * dx + dy * dy);
         }
+
+        public static Result BellmanFord(out List<AStarInfo> data, out List<Vertex<Point>> path, Graph<Point> graph, Point start, Point end)
+        {
+            data = new List<AStarInfo>();
+
+            Queue<Vertex<Point>> queue = new Queue<Vertex<Point>>();
+            Vertex<Point> Start = graph.Search(start);
+            Vertex<Point> End = graph.Search(end);
+
+            if (End == null || Start == null)
+            {
+                path = null;
+                return Result.InvalidPos;
+            }
+
+            path = new List<Vertex<Point>>();
+
+            for (int i = 0; i < graph.Vertices.Count; i++)
+            {
+                queue.Enqueue(graph.Vertices[i]);
+                graph.Vertices[i].Founder = null;
+                graph.Vertices[i].CumulativeDistance = float.PositiveInfinity;
+            }
+
+            Vertex<Point> vertex = queue.Dequeue();
+            vertex.CumulativeDistance = 0;
+
+            path = new List<Vertex<Point>>();
+
+            while (End.Visited == false)
+            {
+                if (queue.Count == 0)
+                {
+                    return Result.NotFound;
+                }
+
+                data.Add(new AStarInfo(ColorToBrush.Visited, vertex.Value));
+
+                for (int i = 0; i < vertex.NeighborCount; i++)
+                {
+                    if (vertex.Neighbors[i].EndingPoint.Visited != true)
+                    {
+                        float tentDist = vertex.CumulativeDistance + vertex.Neighbors[i].Weight;
+
+                        if (tentDist.CompareTo(vertex.Neighbors[i].EndingPoint.CumulativeDistance) < 0)
+                        {
+                            vertex.Neighbors[i].EndingPoint.CumulativeDistance = tentDist;
+
+                            vertex.Neighbors[i].EndingPoint.Founder = vertex;
+                        }
+                        if (vertex.CumulativeDistance.CompareTo(tentDist) > 0)
+                        {
+                            End.Visited = true;
+                        }
+                    }
+                }
+
+                vertex.Visited = true;
+            }
+
+            if (End.Visited == true)
+            {
+                Vertex<Point> finder = End;
+
+                while (finder.Founder != null)
+                {
+                    path.Add(finder);
+
+                    finder = finder.Founder;
+                }
+                path.Add(finder);
+                path.Reverse();
+                for (int i = 0; i < path.Count; i++)
+                {
+                    data.Add(new AStarInfo(ColorToBrush.FinalPath, path[i].Value));
+                }
+            }
+
+            return Result.Found;
+        }
+
         public static Result AStar(out List<AStarInfo> data, out List<Vertex<Point>> path, Graph<Point> graph, Point start, Point end, Heuristic heuristic)
         {
             data = new List<AStarInfo>();
